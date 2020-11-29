51e6cc42d17defba9d3e5a31a2a895bb7060f567
luifa <luifa.luifa@gmail.com>
2020-11-29 23:13:07 +0100

    add fake data for bookings

---
+++ bookingDb.ts
@@ -0,0 +1,15 @@
+interface Booking {
+    id:number,
+    name:string,
+    surname:string,
+    roomNumber:number
+}
+
+export const Bookings:Booking[] =
+[
+    {id:1,name:"Luifa",surname:"Godoy",roomNumber:100},
+    {id:2,name:"Foo",surname:"Bar",roomNumber:121},
+    {id:3,name:"Max",surname:"Mustermann",roomNumber:111},
+    {id:4,name:"Nergal",surname:"Darski",roomNumber:200},
+    {id:5,name:"Till",surname:"Lindemann",roomNumber:105}
+];
